1d9bf00462e01f17740b613ac017243eef3360f7
Kevin Dexter <kevin.dexter@comcast.net>
2018-04-28 14:36:45 -0500

    http calls completed Client side

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -4,4 +4,67 @@ var app = angular.module('TodoApp', []);
 
 app.controller('TodoController', function() {
   console.log('angular.js');
+
+  self = this;
+
+  self.newTask = {};
+  self.tasks = [];
+
+  self.getTasks = function() {
+    $http({
+      method: 'GET',
+      url: '/task'
+    })
+      .then(function (response) {
+        self.tasks = response.data;
+      })
+      .catch(function(error) {
+        console.log('error with /task GET:', error);
+      })
+  }
+
+  self.addTask = function() {
+    $http({
+      method: 'POST', 
+      url: '/task',
+      data: self.newTask
+    })
+      .then(function (response) {
+        self.getTasks();
+      })
+      .catch(function (error) {
+        console.log('error with /task POST', error);
+      })
+  }
+
+  self.completeTask = function(task) {
+    $http({
+      method: 'PUT',
+      url: '/task',
+      data: task
+    })
+      .then(function (response) {
+        self.getTasks();
+      })
+      .catch(function(error) {
+        console.log('error with /task PUT', error);
+      })
+  }
+
+  self.deleteTast = function(tast) {
+    $http({
+      method: 'DELETE',
+      url: '/task',
+      params: task
+    })
+      .then(function (response) {
+        self.getTasks();
+      })
+      .catch(function(error) {
+        console.log('error with /task DELETE', error);
+      })
+  }
+
+  self.getTasks();
+
 })
